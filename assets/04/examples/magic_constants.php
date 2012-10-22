69530d478065d6326a60308befcc582f5a69dbf1
Bramus <bramus@bram.us>
2012-10-22 10:59:09 +0200

    04 typos + ./ explanation fix

---
+++ assets/04/examples/magic_constants.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Magic Constants
+ * @author Bramus Van Damme <bramus.vandamme@kahosl.be>
+ */
+
+	echo '<h2>./</h2>' . PHP_EOL;
+	echo '<strong>dirname(./) :</strong><br /><code>' . dirname('./') . '</code><br />' . PHP_EOL;
+	echo '<strong>realpath(./) :</strong><br /><code>' . realpath('./') . '</code><br />' .PHP_EOL;
+	
+	echo '<h2>__FILE__</h2>' . PHP_EOL;
+	echo '<strong>__FILE__ :</strong><br /><code>' . __FILE__ . '</code><br />' .PHP_EOL;
+	echo '<strong>dirname(__FILE__) :</strong><br /><code>' . dirname(__FILE__) . '</code><br />' .PHP_EOL;
+	
+	echo '<h2>__DIR__</h2>' . PHP_EOL;
+	echo '<strong>__DIR__ :</strong><br /><code>' . __DIR__ . '</code><br />' . PHP_EOL;
+
+// EOF
