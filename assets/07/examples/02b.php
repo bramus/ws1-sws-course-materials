f97292eaedbf15f7acca799368f903e7f57c2e7c
Bramus <bramus@bram.us>
2012-11-19 23:34:28 +0100

    07: errors

--- assets/07/examples/02b.php
+++ assets/07/examples/02b.php
@@ -4,6 +4,6 @@
 	require_once 'config.php';
 
 	// Connect to database server "localhost" with username "root" and a faulty password + select the DB "fotofactory"
-	$dbhandler = @mysqli_connect(DB_HOST, DB_USER, 'wrongpass', DB_NAME_FF) or die ('Could not connect: ' . mysqli_connect_error());
+	$dbhandler = @mysqli_connect(DB_HOST, DB_USER, 'wrongpass', DB_NAME_FF) or die ('Could not connect to database server or access database');
 
 //EOF
