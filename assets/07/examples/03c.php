87a3c70b5b5e629702f9e67974c54b2b91d5f170
Bramus <bramus@bram.us>
2013-10-14 16:37:15 +0200

    07: A note on `$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);`

---
+++ assets/07/examples/03c.php
@@ -0,0 +1,30 @@
+<?php
+
+	/**
+	 * Redirects to the error handling page
+	 * @param string $type
+	 * @return void
+	 */
+	function showDbError($type, $msg) {
+		file_put_contents(__DIR__ . '/error_log_mysql', PHP_EOL . (new DateTime())->format('Y-m-d H:i:s') . ' : ' . $msg, FILE_APPEND);
+		header('location: error.php?type=db&detail=' . $type);
+		exit();
+	}
+
+	// Include config
+	require_once 'config.php';
+
+	// Make Connection
+	try {
+		$db = new PDO('mysql:host=' . DB_HOST .';dbname=' . DB_NAME_FF . ';charset=utf8', DB_USER, DB_PASS);
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	} catch (Exception $e) {
+		showDbError('connect', $e->getMessage());
+	}
+
+	$stmt = $db->exec('DELETE FROM inexistent_table WHERE user_id = 10');
+	var_dump($stmt);
+
+	// Handle result here ....
+
+//EOF
