9005857fe4edc15814ceb9c6a8564e7fbb47fb14
Bramus <bramus@bram.us>
2013-10-20 18:43:06 +0200

    08: Getting Practical (Configuring Twig, Template Inheritance)

---
+++ assets/08/examples/twig/09/index.php
@@ -0,0 +1,19 @@
+<?php
+
+	// includes & requires
+	require_once __DIR__ . '/includes/Twig/Autoloader.php';
+	Twig_Autoloader::register();
+
+	// Twig Bootstrap
+	$loader = new Twig_Loader_Filesystem(__DIR__ . '/templates');
+	$twig = new Twig_Environment($loader);
+
+	// load template
+	$tpl = $twig->loadTemplate('main.twig');
+
+	// render template with our data
+	echo $tpl->render(array(
+		'pageTitle' => 'Template Inheritance'
+	));
+
+// EOF
